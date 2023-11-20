d04163db98b7c71c32c31218da1b9c399f0b2cd1
Vikktor93 <Vikktor93@users.noreply.github.com>
2023-11-20 17:23:41 -0300

    Se agrega modificación "Hola Mundo"

--- holamundo.rb
+++ holamundo.rb
@@ -1,27 +1,10 @@
-#Esto es un Hola Mundo
-print "Hola Mundo!"
-puts "Holi"
 
-contador = 0
+print "¡Hello World!" #Hola Mundo utilizando print (sin salto de linea)
+puts "¡Hola Mundo!" #Hola Mundo utilizando puts (con salto de linea incluido)
 
-#Esto es un bucle
-while contador < 3
-    puts "Iteración #{contador +1}"
-    contador += 1
-end
 
-#Esto es una condicional
-edad = 15
 
-if edad >= 18
-    puts 'Eres mayor de edad, puedes tener licencia'
-else
-    puts 'Eres menor de edad, no puedes tener licencia'
-end    
 
-#Condicional Negativa
-unless edad >= 18
-    ´puts 'Eres menor de edad'
-else
-    puts 'Puedes obtener licencia'
-end
+
+
+
