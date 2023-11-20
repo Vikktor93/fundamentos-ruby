f6f81452f253b6d9f305f7e50f865780e6e8eaa6
Vikktor93 <Vikktor93@users.noreply.github.com>
2023-11-20 17:30:28 -0300

    Se modifica el fichero de bucles

--- bucles.rb
+++ bucles.rb
@@ -1,6 +1,7 @@
 contador = 0
 
 #Esto es un bucle While
+puts("---------- CICLO WHILE ----------")
 while contador < 3
     puts "Iteración #{contador +1}"
     contador += 1
@@ -8,16 +9,17 @@ end
 
 j = 0
 #Esto es un bucle for
+puts("\n\n---------- CICLO FOR ----------")
 for j in 1..5
     puts "Numero #{j}"
 end
 
-#Ciclo Infinito con break
+#Ciclo Infinito + break
 i = 0
+puts("\n\n---------- LOOP + BREAK ----------")
 loop do
-    puts "Numero #{i} en el loop"
+    puts "Numero #{i} del loop"
     i += 1
-
     #condicion para salir del loop
     break if i == 5 
 end
@@ -25,6 +27,7 @@ end
 #Iteración de colecciones con each
 numeros = [1,2,3,4,5]
 
+puts("\n\n---------- CICLO EACH ----------")
 numeros.each do |n|
     puts "Numero del array: #{n}"
 end
