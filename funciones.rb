0f8025c9ede624e40155c40eae5899910266ecce
Vikktor93 <Vikktor93@users.noreply.github.com>
2023-11-20 17:25:57 -0300

    Modificación del fichero Funciones

--- funciones.rb
+++ funciones.rb
@@ -1,5 +1,8 @@
+# Definición de la función 'saludo' que toma un parámetro 'nombre'
 def saludo(nombre)
+    # Imprime un saludo utilizando el valor del parámetro 'nombre'
     puts "Hola mi nombre es #{nombre}"
 end
 
+# Llamada a la función 'saludo' con el argumento "Victor"
 saludo("Victor")
