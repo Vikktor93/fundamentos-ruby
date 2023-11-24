a3a0533ae717a871a5aa0110422dcf7cd6b472bd
Vikktor93 <Vikktor93@users.noreply.github.com>
2023-11-24 13:49:26 -0300

    Se agregan nuevos bucles

--- 06-bucles.rb
+++ 06-bucles.rb
@@ -2,10 +2,19 @@ contador = 0
 
 #Esto es un bucle While
 puts("---------- CICLO WHILE ----------")
-while contador < 3
+while contador <= 5
     puts "Iteración #{contador +1}"
     contador += 1
 end
+
+newcontador = 0
+#Until: Ejecuta un bloque de código mientras una condición sea falsa
+puts("\n\n---------- CICLO UNTIL ----------")
+until newcontador == 5 do
+  puts "Iteración #{newcontador}"
+  newcontador += 1
+end
+
 
 j = 0
 #Esto es un bucle for
@@ -30,4 +39,10 @@ numeros = [1,2,3,4,5]
 puts("\n\n---------- CICLO EACH ----------")
 numeros.each do |n|
     puts "Numero del array: #{n}"
+end
+
+#Método Times: Ejecuta un bloque de código una cantidad específica de veces.
+puts("\n\n---------- CICLO TIMES ----------")
+5.times do |x|
+  puts "Ciclo: #{x}"
 end
